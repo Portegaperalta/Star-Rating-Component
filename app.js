11c00ebeda0a6a8c533d79612e756fcd89413fd2
pablothecoder <ortegaperaltap@gmail.com>
2024-11-09 17:13:34 -0400

    html css and js work

---
+++ app.js
@@ -0,0 +1,38 @@
+const stars = document.querySelectorAll('div .stars i')
+
+const starOne = document.querySelector('.star-one')
+const starTwo = document.querySelector('.star-two')
+const starThree = document.querySelector('.star-three')
+const starFour = document.querySelector('.star-four')
+const starFive = document.querySelector('.star-five')
+
+const starText = document.querySelector('.star-text')
+const starTexts = [
+  {
+    text: "Excellent! We're thrilled to hear you had such a positive experience. thank you for choosing our platform"
+  },
+  {
+    text: "Thank you for your positive feedback! We're glad to know that you had a great experience and we appreciate your support."
+  },
+  {
+    text: "Thank you for your feedback.We're sorry to hear that your experience wasn't perfect. We would love to hear more about your concerns to see how we can improve"
+  },
+  {
+    text: "We apologize for the inconvenience your experienced. We appreciate your feedback and would like to work with you to address any issues."
+  },
+  {
+    text: "We're sorry to hear that you had a bad experience. We would like to learn more about what happened and how we can make things right."
+  }
+]
+
+// hover on star effect
+
+for (let i = 0; i < stars.length; i++) {
+  stars[i].addEventListener('mouseover', () => {
+    console.log('working')
+  })
+}
+
+// change text depending star rating
+
+starOne
