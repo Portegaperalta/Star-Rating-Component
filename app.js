55626a822ddc9b3913854b57481062d6e7705608
pablothecoder <ortegaperaltap@gmail.com>
2024-11-09 18:08:03 -0400

    added final mouse over star effects

--- app.js
+++ app.js
@@ -33,24 +33,82 @@ for (let i = 0; i < stars.length; i++) {
   })
 }
 
+for (let i = 0; i < stars.length; i++) {
+  stars[i].addEventListener('mouseout', () => {
+    stars[i].style.color = "#E2E8F0"
+  })
+}
+
 // change text depending star rating
 
 starOne.addEventListener('click', () => {
   starText.innerText = starTexts[4].text
 })
 
+// star two hover effects
+
 starTwo.addEventListener('click', () => {
   starText.innerText = starTexts[3].text
 })
+
+starTwo.addEventListener('mouseover', () => {
+  starOne.style.color = '#FACC15'
+})
+
+starTwo.addEventListener('mouseout', () => {
+  starOne.style.color = '#E2E8F0'
+})
+
+//star three hover effects
 
 starThree.addEventListener('click', () => {
   starText.innerText = starTexts[2].text
 })
 
+starThree.addEventListener('mouseover', () => {
+  starOne.style.color = '#FACC15'
+  starTwo.style.color = '#FACC15'
+})
+
+starThree.addEventListener('mouseout', () => {
+  starOne.style.color = '#E2E8F0'
+  starTwo.style.color = '#E2E8F0'
+})
+
+//star four hover effects
+
 starFour.addEventListener('click', () => {
   starText.innerText = starTexts[1].text
 })
 
+starFour.addEventListener('mouseover', () => {
+  starOne.style.color = '#FACC15'
+  starTwo.style.color = '#FACC15'
+  starThree.style.color = '#FACC15'
+})
+
+starFour.addEventListener('mouseout', () => {
+  starOne.style.color = '#E2E8F0'
+  starTwo.style.color = '#E2E8F0'
+  starThree.style.color = '#E2E8F0'
+})
+
+// star five hover effects
+
 starFive.addEventListener('click', () => {
   starText.innerText = starTexts[0].text
+})
+
+starFive.addEventListener('mouseover', () => {
+  starOne.style.color = '#FACC15'
+  starTwo.style.color = '#FACC15'
+  starThree.style.color = '#FACC15'
+  starFour.style.color = '#FACC15'
+})
+
+starFive.addEventListener('mouseout', () => {
+  starOne.style.color = '#E2E8F0'
+  starTwo.style.color = '#E2E8F0'
+  starThree.style.color = '#E2E8F0'
+  starFour.style.color = '#E2E8F0'
 })
